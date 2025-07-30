2b822ae11f070d725e1ae0ecf95e065ff9b6047f
mrvikky17 <114817912+mrvikky17@users.noreply.github.com>
2025-07-30 13:13:15 +0530

    Update hello.java

--- Day1_ProgrammingElements/hello.java
+++ Day1_ProgrammingElements/hello.java
@@ -3,6 +3,8 @@ package Day1_ProgrammingElements;
 public class hello {
     public static void main(String[] args) {
         
-        System.err.println("hello");
+        System.out.println("hello");
+        System.out.println("World");
+        
     }
 }
