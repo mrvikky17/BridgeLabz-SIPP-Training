bf043f4f9c977e03dcbb0bdb427056789e764afb
mrvikky17 <vikkykushwah172003@gmail.com>
2025-08-13 16:06:22 +0530

    [Vikky kushwah Add:check divisile by 5]

---
+++ Day1_ProgrammingElements/JavaControlFlow/checkNumberDivisibleBy5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class checkNumberDivisibleBy5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if(number % 5 == 0){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        sc.close();
+    }
+}
