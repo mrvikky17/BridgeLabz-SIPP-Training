c444ef2046264ab35894a2c6f00f4c230cc7f42c
mrvikky17 <vikkykushwah172003@gmail.com>
2025-08-13 16:19:16 +0530

    [Vikky kushwah Add:find factor]

---
+++ Day1_ProgrammingElements/JavaControlFlow/findFactor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class findFactor {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        sc.close();
+    }
+    
+}
