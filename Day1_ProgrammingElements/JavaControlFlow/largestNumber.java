bbf8837d9c8474c3bf0110a408cd6ab446ef2a88
mrvikky17 <vikkykushwah172003@gmail.com>
2025-08-13 16:25:24 +0530

    [Vikky kushwah Add:largest number]

---
+++ Day1_ProgrammingElements/JavaControlFlow/largestNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class largestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        int number3 = sc.nextInt();
+        if (number1 >= number2 && number1 >= number3) {
+            System.out.println(number1 + " is the largest");
+        } else if (number2 >= number1 && number2 >= number3) {
+            System.out.println(number2 + " is the largest");
+        } else {
+            System.out.println(number3 + " is the largest");
+        }
+        sc.close();
+    }
+}
