9ddcb142854cec3e86593ba80856b8aeea1a104a
mrvikky17 <vikkykushwah172003@gmail.com>
2025-08-19 21:05:50 +0530

    [Vikky kushwah Add:check spring season]

---
+++ Day1_ProgrammingElements/Methods/SpringSeason.java
@@ -0,0 +1,26 @@
+package Methods;
+import java.util.*;
+
+public class SpringSeason {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        if (isSpringSeason(month, day)) {
+            System.out.println("Spring Season");
+        } else {
+            System.out.println("not Spring Season");
+        }
+        sc.close();
+    }
+
+    public static boolean isSpringSeason(int month, int day) {
+        if ((month == 3 && day >= 20 && day <= 31) ||
+            (month == 4 && day >= 1 && day <= 30) ||
+            (month == 5 && day >= 1 && day <= 31) ||
+            (month == 6 && day >= 1 && day <= 20)) {
+            return true;
+        }
+        return false;
+    }
+}
